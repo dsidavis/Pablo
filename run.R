eef6e9702a99a51634800752dc81848f94e1f07b
Duncan Temple Lang <dtemplelang@ucdavis.edu>
2017-05-08 07:01:33 -0700

    show how to call with different inputs

--- run.R
+++ run.R
@@ -6,3 +6,8 @@ Rprof("prof")
 system.time(doSim(beta_list, rho_list, D, time, v, D_imm, Farms, nsims = 1, verbose = TRUE))
 Rprof(NULL)
 summaryRprof('prof')$by.self
+
+
+system.time(doSim(beta_listV25, rho_list_F80_25, D25, time, v, D_imm, Farms, nsims = 1, verbose = TRUE))
+
+
